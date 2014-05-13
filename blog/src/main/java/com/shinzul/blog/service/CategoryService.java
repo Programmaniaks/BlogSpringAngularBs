24dabecd978ec29a366ada85751a53a4350ec431
Shinzul <Shinzul@Shinzul-PC>
2014-05-13 22:42:00 +0200

    Add first service.

---
+++ blog/src/main/java/com/shinzul/blog/service/CategoryService.java
@@ -0,0 +1,33 @@
+package com.shinzul.blog.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import com.google.common.collect.Lists;
+import com.shinzul.blog.dao.CategoryRepository;
+import com.shinzul.blog.entity.Category;
+
+@Service
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+
+	public CategoryRepository getCategoryRepository() {
+		return categoryRepository;
+	}
+
+	public void setCategoryRepository(CategoryRepository categoryRepository) {
+		this.categoryRepository = categoryRepository;
+	}
+	
+	public List<Category> getAllCategories() {
+		return Lists.newArrayList(getCategoryRepository().findAll());
+	}
+
+}
