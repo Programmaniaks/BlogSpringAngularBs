2f167344030283148cc37b16f2a94f7c4661c9d9
Programmaniaks <programmaniaks@gmail.com>
2015-05-18 00:59:02 +0200

    Migrate to SpringBoot to avoid tomcat management things.

---
+++ blog/src/main/java/com/shinzul/blog/Application.java
@@ -0,0 +1,32 @@
+package com.shinzul.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.DispatcherServlet;
+
+@SpringBootApplication
+public class Application {
+	
+	public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+//        System.out.println("Let's inspect the beans provided by Spring Boot:");
+//
+//        String[] beanNames = ctx.getBeanDefinitionNames();
+//        Arrays.sort(beanNames);
+//        for (String beanName : beanNames) {
+//            System.out.println(beanName);
+//        }
+    }
+
+    @Bean
+    public ServletRegistrationBean dispatcherRegistration(DispatcherServlet dispatcherServlet) {
+        ServletRegistrationBean registration = new ServletRegistrationBean(
+                dispatcherServlet);
+        registration.addUrlMappings("/services/*");
+        return registration;
+    }
+
+}
--- blog/src/main/java/com/shinzul/blog/configuration/WebConfig.java
+++ blog/src/main/java/com/shinzul/blog/configuration/WebConfig.java
@@ -2,12 +2,14 @@ package com.shinzul.blog.configuration;
 
 import java.util.List;
 
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperFactoryBean;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
